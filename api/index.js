9b50c0c4a982c5b7b401d2c4e8799bc10144230d
T4nae <tan.b63000@gmail.com>
2023-01-29 17:00:16 +0530

    cors and middleware fixed

--- api/index.js
+++ api/index.js
@@ -1,16 +1,32 @@
-require('dotenv').config()
+require("dotenv").config();
+var bodyParser = require("body-parser");
 
 const express = require("express");
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const session = require("express-session");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
 let PORT;
-process.env.STATUS === "production" ? (PORT = process.env.PROD_PORT) : (PORT = process.env.DEV_PORT);
-console.log(PORT);
+process.env.STATUS === "production"
+    ? (PORT = process.env.PROD_PORT)
+    : (PORT = process.env.DEV_PORT);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require("cors")());
+
+app.use(
+    session({
+        secret: JWT_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use("/portfolio", require("./routes/portfolio"));
 app.use("/", require("./routes/auth"));
-app.use("/", require("./routes/portfolio"));
 
 const Start = async () => {
     try {
