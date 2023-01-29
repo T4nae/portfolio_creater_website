9c97b684ba80cc844319a79dddcced8edc686c1b
T4nae <tan.b63000@gmail.com>
2023-01-29 17:02:22 +0530

    Nav added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 import Nav from "./components/nav";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Portfolio from "./pages/Portfolio"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Portfolio from "./pages/Portfolio";
+import PortfolioEdit from "./pages/portfolioEdit";
 import NotFound from "./pages/NotFound";
 
-function App() {
+function App(props) {
     return (
         <div className="App">
             <Routes>
@@ -19,7 +21,7 @@ function App() {
                     path="/"
                     element={
                         <div>
-                            <Nav />
+                            <Nav proxy={props.proxy} />
                             <Home />
                         </div>
                     }
@@ -28,7 +30,7 @@ function App() {
                     path="about"
                     element={
                         <div>
-                            <Nav />
+                            <Nav proxy={props.proxy} />
                             <About />
                         </div>
                     }
@@ -36,36 +38,47 @@ function App() {
                 <Route
                     path="contact"
                     element={
-                        <div>
-                            <Nav />
+                        <>
+                            <Nav proxy={props.proxy} />
                             <Contact />
-                        </div>
+                        </>
                     }
                 />
-				<Route
-					path="login"
-					element={
-						<Login />
-					}
-				/>
-				<Route 
-					path="register"
-					element={
-						<Register />
-					}
-				/>
-				<Route 
-					path="portfolio"
-					element={
-						<Portfolio />
-					}
-				/>
-				<Route
-					path="*"
-					element={
-						<NotFound />
-					}
-				/>
+                <Route
+                    path="login"
+                    element={
+                        <>
+                            <Nav proxy={props.proxy} />
+                            <Login proxy={props.proxy} />
+                        </>
+                    }
+                />
+                <Route
+                    path="register"
+                    element={
+                        <>
+                            <Nav proxy={props.proxy} />
+                            <Register proxy={props.proxy} />
+                        </>
+                    }
+                />
+                <Route
+                    path="portfolio/:username"
+                    element={<Portfolio proxy={props.proxy} />}
+                />
+                <Route
+                    path="portfolio/:username/edit"
+                    element={<PortfolioEdit proxy={props.proxy} />}
+                />
+                <Route
+                    path="*"
+                    element={
+                        <>
+                            <Nav proxy={props.proxy} />
+                            <NotFound />
+                        </>
+                    }
+                />
             </Routes>
         </div>
     );
