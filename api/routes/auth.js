2818775a70300e13dc87a21e16d3f32d30e05b52
T4nae <tan.b63000@gmail.com>
2023-01-29 17:01:38 +0530

    auth added and fixed

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const controller = require('../controllers/auth.controller');
-const middleware = require('../middleware/auth.middleware');
+const express = require("express");
+const controller = require("../controllers/auth.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 const auth = express.Router();
-
-auth.use(middleware)
 
 auth.post("/register", controller.register);
 auth.post("/login", controller.login);
-auth.get("/logout", controller.logout);
+auth.use("/auth/*", authMiddleware);
+auth.post("/auth/logout", controller.logout);
 
 module.exports = auth;
--- api/routes/portfolio.js
+++ api/routes/portfolio.js
@@ -3,10 +3,9 @@ const controller = require("../controllers/portfolio.controller");
 const middleware = require("../middleware/auth.middleware");
 const portfolio = express.Router();
 
-portfolio.get("/portfolio/:user", controller.getPortfolio);
-
-portfolio.use("/portfolio/auth",middleware);
-portfolio.post("/portfolio/auth/:user", controller.createPortfolio);
-portfolio.put("/portfolio/auth/:user/edit", controller.updatePortfolio);
+portfolio.get("/:user", controller.getPortfolio);
+portfolio.use("/auth/*", middleware);
+portfolio.post("/auth/create/:user", controller.createPortfolio);
+portfolio.put("/auth/edit/:user", controller.updatePortfolio);
 
 module.exports = portfolio;
