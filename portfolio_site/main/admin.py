d18733c750726a95f6e738e6106e788d17365a25
T4nae <tan.b63000@gmail.com>
2023-02-18 15:24:35 +0530

    removed custom user

--- portfolio_site/main/admin.py
+++ portfolio_site/main/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Portfolio
+from .models import Portfolio
 
-admin.site.register(User)
 admin.site.register(Portfolio)
--- portfolio_site/main/models.py
+++ portfolio_site/main/models.py
@@ -1,19 +1,10 @@
 from django.db import models
-
-# Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.username
+from django.conf import settings
 
 
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     content = models.TextField()
